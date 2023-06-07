794fd2e526c263f6d0aaad8fa236a743de13a0a4
procape <codigo.procape@upe.br>
2023-06-07 08:47:25 -0300

    Novo arquivo para bkp

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>teste</title>
+</head>
+
+<body>
+    <h1>PAGINA TESTE</h1>
+
+    <!-- <div class="col-md-3">
+	    <div class="form-group">
+			<label for="senha"><strong>Senha</strong></label>
+			<input type="password" placeholder="senha do windows" name="senha_sol" class="form-control" required="">
+	    </div> -->
+    </div>
+</body>
+
+</html>
